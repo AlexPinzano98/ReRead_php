b0adf9c44f98bf71f15fe6d96567204299426fdf
AlexPinzano98 <71654957+AlexPinzano98@users.noreply.github.com>
2020-10-05 16:56:15 +0200

    Add files via upload

---
+++ services/connection.php
@@ -0,0 +1,29 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "reread";
+// Crear la conexión a la base de datos
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+// Checkear la conexión
+if (!$conn) {
+    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+    echo "Error de depuración: " . mysqli_connect_errno() . PHP_EOL;
+    exit;
+} else {
+	mysqli_set_charset($conn, "utf8");
+}
+
+/* //CONEXION A LA BASE DE DATOS -> ORIENTADO A OBJETOS
+// Crear la conexión
+$conn = new mysqli('localhost', 'root', '', 'imagenes');
+// Checkear la conexión
+if ($conn->connect_errno) {
+	die("Error: No se pudo conectar a MySQL: " . $conn->connect_errno);
+} else {
+	$conn->set_charset("utf8");
+}
+*/
+
+?>
